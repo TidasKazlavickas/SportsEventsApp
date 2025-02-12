a67cde33ca84845ca8475f74fb2daf53d3f05b0b
TidasKazlavickas <tidasas007@gmail.com>
2025-02-12 21:05:33 +0200

    perdaryta projekto struktura kad veiktu su azure

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tidas\OneDrive\Stalinis kompiuteri\SportsEventsApp-main\venv
+set VIRTUAL_ENV=C:\Users\tidas\OneDrive\Stalinis kompiuteri\SportsEventsApp-main\SportsEventApp\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
